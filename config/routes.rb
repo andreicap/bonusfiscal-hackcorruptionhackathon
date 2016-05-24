378eb41cb4975b509151d70c8975b1d13e361214
andreicap <andrei.capastru@gmail.com>
2016-05-24 14:30:53 +0300

    Fix Edit/New devise partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  devise_scope :user do 
+    get 'logout' => 'devise/sessions#destroy'
+  end
+
   root 'landing#index'
 
   get '/info', to: 'pages#index'
