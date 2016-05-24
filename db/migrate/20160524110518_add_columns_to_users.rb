cf421d46ecce6658384f31317d4bd303dde39579
andreicap <andrei.capastru@gmail.com>
2016-05-24 14:07:17 +0300

    Make Alert Layout, add provider, uid, first/last name to users

--- db/migrate/20160524110518_add_columns_to_users.rb
+++ db/migrate/20160524110518_add_columns_to_users.rb
@@ -2,7 +2,7 @@ class AddColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
+    add_column :users, :first_name, :string, :default => nil
+    add_column :users, :last_name, :string, :default => nil
   end
 end
