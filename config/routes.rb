1b0ec13d9b223f17b3a161ad28be5bf2ae2774e1
andreicap <andrei.capastru@gmail.com>
2016-05-25 19:48:09 +0300

    Modify root to pages/index

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/auth/:provider/callback' => 'authentications#create'
   resources :authentications, only: [:create, :destroy]
   get 'authentications' => 'authentications#index'
-  root 'authentications#index'
+  root 'pages#index'
 
   get '/info', to: 'pages#index'
 
