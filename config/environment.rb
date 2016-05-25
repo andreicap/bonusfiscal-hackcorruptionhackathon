8d660102e99690b5c69ec72d167726f408c26235
andreicap <andrei.capastru@gmail.com>
2016-05-25 17:11:10 +0300

    Small Fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-
-# Load heroku vars from local file
-# heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-
 # Initialize the Rails application.
 Rails.application.initialize!
