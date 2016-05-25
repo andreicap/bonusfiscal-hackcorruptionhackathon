d989d8e4ba0af34cffc20ec8568c1bb46f63c14d
andreicap <andrei.capastru@gmail.com>
2016-05-25 16:26:23 +0300

    Fuck env variables

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 
 # Load heroku vars from local file
-heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-load(heroku_env) if File.exists?(heroku_env)
+# heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
 
 # Initialize the Rails application.
 Rails.application.initialize!
