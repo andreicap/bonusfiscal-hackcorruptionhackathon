3701a2300cfdf56eb765538aff9f0cd4072cc8a3
andreicap <andrei.capastru@gmail.com>
2016-06-02 18:15:24 +0300

    Add ability to get and display tweets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,33 @@
-
+require 'pp'
 class PagesController < ApplicationController
   before_action :set_user
   @name = "You did not provide any name yet"
   
   def index
-    EasyTranslate.api_key = 'AIzaSyB1r3abwQulFPKY_RpduJlonl-x0wHLy7w'
+    #EasyTranslate.api_key = 'AIzaSyB1r3abwQulFPKY_RpduJlonl-x0wHLy7w'
+
     if @user.facebook
-      @access_token = @user.authentications.find_by_provider(:facebook).token
+      @access_token = get_fb_auth.token
       puts @graph = Koala::Facebook::API.new(@access_token)
       @name = get_fb_auth.name;
-      @feed = @graph.get_connection("RailsGirls.Chisinau", "posts")
+      @fb_feed = @graph.get_connection("RailsGirls.Chisinau", "posts")
     end
 
     if @user.twitter
-      
+      @client_tw = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_SECRET"]
+        config.access_token        = get_tw_auth.token
+        config.access_token_secret = get_tw_auth.secret
+      end
+      @tw_feed = @client_tw.user_timeline      
     end
-    
-   end
+
+  end
 
 
-   private
-   def get_fb_auth
+  private
+  def get_fb_auth
     @user.authentications.find_by_provider(:facebook)
   end
 
