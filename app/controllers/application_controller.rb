c5093aededc37f179ad69bc1c17d3e60891156c8
andreicap <andrei.capastru@gmail.com>
2016-07-05 18:04:22 +0300

    init branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :authenticate_user!, :only => [:index]
 end
