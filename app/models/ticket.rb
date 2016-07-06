a83e03fe5d839098bece22da2dd3e4a0b3cfec9d
andreicap <andrei.capastru@gmail.com>
2016-07-06 17:24:50 +0300

    Add ticket creation logic and views

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,3 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
+  belongs_to :citizen
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705150521) do
+ActiveRecord::Schema.define(version: 20160706090132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160705150521) do
     t.string   "company",       default: ""
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "citizen_id"
   end
 
 end
