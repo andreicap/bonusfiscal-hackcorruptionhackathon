82008d0d19931da210029715dcbaaca12b8b0e9d
andreicap <andrei.capastru@gmail.com>
2016-07-06 11:05:24 +0300

    Add partial navbar, edit registration

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,4 +1,5 @@
 class Citizen < ApplicationRecord
+  has_many :tickets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
