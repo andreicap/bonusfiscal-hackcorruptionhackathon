651f4f4418c8d09df71c779405cacd0143549528
Andrei Capastru <andreicap@users.noreply.github.com>
2016-07-07 10:32:10 +0300

    Update ticket.rb

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :citizen
 
 
-  validates :company_idno, :nr_bon_fiscal, :price, :company, :data_eliberarii, :presence => { :message => "Cîmpul este obligatoriu de completat"}
-  validates :company_idno, :length => { :minimum => 13, :message => "Nr. minim de cifre este 13 pentru cîmpul dat" }
-  validates :company_idno, :nr_bon_fiscal, :price, :numericality => { :message => "Introduceti doar numere" } 
+  validates :company_idno, :nr_bon_fiscal, :price, :company, :data_eliberarii, :presence => { :message => "Câmpul este obligatoriu de completat"}
+  validates :company_idno, :length => { :minimum => 13, :message => "Numărul minim de cifre este 13 pentru câmpul dat" }
+  validates :company_idno, :nr_bon_fiscal, :price, :numericality => { :message => "Introduceți doar cifre" } 
 end
