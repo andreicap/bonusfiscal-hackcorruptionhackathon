e1f5aa6a06baead344d68f5dcbd06596d6fa2da3
CristianChris <cristian.cartofeanu@gmail.com>
2016-07-07 10:29:07 +0300

    add date validator

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :citizen
 
-# TO DO: Add :data_eliberarii in 1 validation
-  validates :company_idno, :nr_bon_fiscal, :price, :company, :presence => { :message => "Cîmpul este obligatoriu de completat"}
+
+  validates :company_idno, :nr_bon_fiscal, :price, :company, :data_eliberarii, :presence => { :message => "Cîmpul este obligatoriu de completat"}
   validates :company_idno, :length => { :minimum => 13, :message => "Nr. minim de cifre este 13 pentru cîmpul dat" }
   validates :company_idno, :nr_bon_fiscal, :price, :numericality => { :message => "Introduceti doar numere" } 
 end
