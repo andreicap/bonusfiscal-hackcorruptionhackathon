b9664658458519f30ddb6951a58d49fa916490d2
CristianChris <cristian.cartofeanu@gmail.com>
2016-07-07 10:24:38 +0300

    add validation for ticket form

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,8 +1,7 @@
 class Citizen < ApplicationRecord
-  has_many :tickets
+	has_many :tickets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
-         #,:validatable
+  :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706151555) do
+ActiveRecord::Schema.define(version: 20160707062522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20160706151555) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "citizen_id"
-    t.date     "data_eliberarii"
+    t.string   "data_eliberarii"
   end
 
 end
