01ed009425c3f96c81d454bad5b332f7f8032402
andreicap <andrei.capastru@gmail.com>
2016-07-07 12:51:31 +0300

    Add randomizer

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -1,13 +1,40 @@
 class LotteryController < ApplicationController
   skip_before_action :authenticate_citizen!, :only => [:index]
+  before_action :set_tickets, :set_winners
 
   def index
-    
+ 
   end
 
   def generate_winners
-    puts "generator ============================"
-    redirect_to '/lottery'
+    winner_generator
+  end
+
+private
+  
+  def winner_generator
+
+    ids = @tickets.ids
+    ids_that_won = @winners.ids
+    ids_pool = ids-ids_that_won
+    if ids_pool.empty?
+      flash[:notice] = "Tichetele s-au consumat"
+      redirect_to '/lottery'
+    else
+      winner_id = (ids_pool).sample
+      w = Winner.new
+      w.ticket = @tickets.find(winner_id)
+      w.save
+      redirect_to '/lottery'
+    end
+  end
+
+  def set_tickets
+    @tickets = Ticket.all
+  end
+
+  def set_winners
+    @winners = Winner.all
   end
   
 end
