af45ecc6b97298d81c6b9d6363e5237d52330544
Daniel Macrinici <macrinici.d@gmail.com>
2016-07-09 09:24:25 +0300

    Add price constraint

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,7 +4,8 @@ class Ticket < ApplicationRecord
 
 
 
-  validates :company_idno, :nr_bon_fiscal, :price, :company, :data_el, :presence => { :message => "Câmpul este obligatoriu."}
-  validates :company_idno, :length => { :minimum => 13, :message => "Numărul minim de cifre este 13 pentru câmpul dat." }
+  validates :company_idno, :nr_bon_fiscal, :price, :company, :data_el, :presence => {:message => "Câmpul este obligatoriu."}
+  validates :company_idno, :length => {:minimum => 13, :message => "Numărul minim de cifre este 13 pentru câmpul dat." }
   validates :company_idno, :nr_bon_fiscal, :price, :numericality => { :message => "Introduceți doar cifre." }
+  validates :price, :numericality => {:greater_than_or_equal_to => 5, :message => "Prețul minim pentru bilet este 5 lei." }
 end
