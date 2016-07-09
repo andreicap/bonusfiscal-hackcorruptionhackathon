853829eb0e0d49f20c7cce417a263777a23b2f74
andreicap <andrei.capastru@gmail.com>
2016-07-09 12:04:08 +0300

    Config privateemail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :address              => 'smtp.privateemail.com',
+    :address              => 'mail.privateemail.com',
     :port                 =>  26,
     :user_name            => 'support@bonusfiscal.me',
     :password             => 'generator123',
