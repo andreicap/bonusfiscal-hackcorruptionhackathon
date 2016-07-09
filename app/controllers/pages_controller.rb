8935fb92f1230fe76ec3150538bcced801d8bdbf
CristianChris <cristian.cartofeanu@gmail.com>
2016-07-09 11:00:31 +0300

    add static report page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_citizen!, :only => [:index]
+  skip_before_action :authenticate_citizen!, :only => [:index, :report]
   def index
     
+  end
+
+  def report
+
   end
 end
