5fdc9eafbc0f728479806c07f2a73b0da7db4916
andreicap <andrei.capastru@gmail.com>
2016-07-09 12:49:50 +0300

    Config privateemail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "hackcorruption_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'bonusfiscal.me:80' }
+  config.action_mailer.default_url_options = { host: 'lotterygov.herokuapp.com:80' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", 
