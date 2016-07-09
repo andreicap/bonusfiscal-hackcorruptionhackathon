ad1f160675bcad6895af93a61a368e9517eb6f1e
andreicap <andrei.capastru@gmail.com>
2016-07-09 11:27:23 +0300

    Config privateemail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,14 +59,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'lotterygov.herokuapp.com:80' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", 
-                  :port => 587,
-                  :domain => 'gmail.com',
-                  :authentication => :login,
-                  :user_name => "bonusfiscal@gmail.com",
-                  :password => "generator123",
-                  :enable_starttls_auto => true
-                }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'mail.privateemail.com',
+    :port                 =>  26,
+    :user_name            => 'support@bonusfiscal.me',
+    :password             => 'generator123',
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
