8a62eec47c273eb3d4bb575dc20dbdaa9778e3a1
andreicap <andrei.capastru@gmail.com>
2016-07-10 05:06:51 +0300

    Fix viewports | Make Landing | FIx dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require jquery.turbolinks
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
-//= require jquery_ujs
 //= require_tree .
 
 
