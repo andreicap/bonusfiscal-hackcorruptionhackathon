2e828df067ab8495294597974806395c7a1de175
Daniel Macrinici <macrinici.d@gmail.com>
2016-07-10 12:51:59 +0300

    Add submissions functionality, change schema, adjust UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709222521) do
+ActiveRecord::Schema.define(version: 20160710065121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,25 @@ ActiveRecord::Schema.define(version: 20160709222521) do
     t.index ["confirmation_token"], name: "index_citizens_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_citizens_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_citizens_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "submission_tables", force: :cascade do |t|
+    t.string   "institution_name"
+    t.string   "city"
+    t.string   "address"
+    t.string   "description"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "submissions", force: :cascade do |t|
+    t.string   "institution_name"
+    t.string   "city"
+    t.string   "address"
+    t.string   "description"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "citizen_id"
   end
 
   create_table "tickets", force: :cascade do |t|
