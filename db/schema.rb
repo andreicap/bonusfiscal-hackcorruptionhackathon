83fd72ad3e61cac3fec123d17ab26283c4331682
CristianChris <cristian.cartofeanu@gmail.com>
2016-07-10 13:08:33 +0300

    change color buttons

--- db/schema.rb
+++ db/schema.rb
@@ -39,15 +39,6 @@ ActiveRecord::Schema.define(version: 20160710065121) do
     t.index ["reset_password_token"], name: "index_citizens_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "submission_tables", force: :cascade do |t|
-    t.string   "institution_name"
-    t.string   "city"
-    t.string   "address"
-    t.string   "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "submissions", force: :cascade do |t|
     t.string   "institution_name"
     t.string   "city"
