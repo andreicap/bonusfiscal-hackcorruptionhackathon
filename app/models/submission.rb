6327d9414a3fc8e31dd417f17ffc89bca43ec4f1
CristianChris <cristian.cartofeanu@gmail.com>
2016-07-15 17:06:52 +0300

    validate report page

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,3 @@
 class Submission < ApplicationRecord
-
+	validates :institution_name, :city, :address, :description, :length => { :minimum => 5, :message => "Numărul minim de caractere este 5 pentru câmpul dat" }
 end
