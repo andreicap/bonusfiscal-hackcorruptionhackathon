dec4e4cd9ab5a3c18495918b05415940b05f478c
andreicap <andrei.capastru@gmail.com>
2016-07-19 15:11:41 +0300

    Add titleing to models

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,9 @@
 class Guest < ApplicationRecord
   has_many :tickets
+  before_save :titleing
+
   def titleing
-  	self[:name].titleize
-  	self[:surname].titleize
+  	self.name = self.name.split.map(&:capitalize).join(' ')
+  	self.surname = self.surname.split.map(&:capitalize).join(' ')
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,12 +3,17 @@ class Ticket < ApplicationRecord
   belongs_to :citizen
   belongs_to :guest
   has_one :winner, dependent: :destroy
+  before_save :titleing
 
   validates :company_idno, :company_mcc, :nr_bon_fiscal, :price, :company, :data_el, :presence => { :message => "Câmpul este obligatoriu"}
   validates :company_idno, :length => { :minimum => 13, :message => "Numărul minim de cifre este 13 pentru câmpul dat" }
   validates :company_idno, :nr_bon_fiscal, :price, :numericality => { :message => "Introduceți doar cifre" }
   validates :price, :numericality => {:greater_than_or_equal_to => 5, :message => "Prețul minim pentru bilet este 5 lei." }
   validate  :ticket_quota, :on => :create
+
+  def titleing
+    self.company = self.company.split.map(&:capitalize)*" "
+  end
 
 private
 
